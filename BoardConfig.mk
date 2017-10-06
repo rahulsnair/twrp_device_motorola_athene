0edabf405c9ed47249025c217781c7eed74721ce
rahulsnair <rahulsnair30@gmail.com>
2017-10-06 22:39:00 +0000

    athene : set release version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,3 +70,5 @@ TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_THEME := portrait_hdpi
 TW_INPUT_BLACKLIST := "hbtp_vm"
+
+TW_DEVICE_VERSION := O1-athene by rahulsnair
